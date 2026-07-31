6644b3da7f3414d7d77d3a1616835383929e6d1e
Jamileiro <jamileiro@gmail.com>
2026-07-31 16:29:11 -0300

    fix: recompila Tailwind v4 completo e corrige service worker para caminhos relativos

--- sw.js
+++ sw.js
@@ -1,18 +1,21 @@
 /* ConsultaCNPJ - Service Worker */
-const CACHE = "ccnpj-v1";
+const CACHE = "ccnpj-v2";
 const ASSETS = [
-  "/",
-  "/index.html",
-  "/static/css/tailwind.min.css",
-  "/static/css/style.css",
-  "/static/js/app.js",
-  "/manifest.json",
-  "/static/img/icon-192.png",
-  "/static/img/icon-512.png",
+  "./",
+  "./index.html",
+  "./static/css/tailwind.min.css",
+  "./static/css/style.css",
+  "./static/js/app.js",
+  "./manifest.json",
+  "./static/img/icon-192.png",
+  "./static/img/icon-512.png",
 ];
 
 self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS).catch(() => {})),
+  );
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (e) => {
@@ -23,17 +26,20 @@ self.addEventListener("activate", (e) => {
         Promise.all(
           keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)),
         ),
-      ),
+      )
+      .then(() => self.clients.claim()),
   );
 });
 
 self.addEventListener("fetch", (e) => {
   if (e.request.method !== "GET") return;
+  // Não interceptar chamadas para a API externa (publica.cnpj.ws)
+  if (e.request.url.indexOf("publica.cnpj.ws") !== -1) return;
   e.respondWith(
     caches.match(e.request).then((cached) => {
       const fetchPromise = fetch(e.request)
         .then((response) => {
-          if (response.ok && response.type === "basic") {
+          if (response && response.ok) {
             const copy = response.clone();
             caches.open(CACHE).then((cache) => cache.put(e.request, copy));
           }
@@ -44,3 +50,5 @@ self.addEventListener("fetch", (e) => {
     }),
   );
 });
+</content>
+
